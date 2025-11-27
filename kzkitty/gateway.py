15c880d3752374660665e6f3aa533a3f9fedc922
catmint <catmint@kittyware.cc>
2025-11-27 04:55:27 -0600

    Change initial players to default players and support incremental imports

--- kzkitty/gateway.py
+++ kzkitty/gateway.py
@@ -14,19 +14,20 @@ class GatewayBot(hikari.GatewayBot):
     async def start(self, *args, **kwargs) -> None:
         await init_db()
 
-        initial_user_file = os.environ.get('KZKITTY_INITIAL_PLAYERS')
-        if initial_user_file is not None:
-            if await Player.all().count() == 0:
-                users = []
-                with open(initial_user_file, newline='') as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        users.append(Player(id=int(row['id']),
+        default_player_file = os.environ.get('KZKITTY_DEFAULT_PLAYERS')
+        if default_player_file is not None:
+            users = []
+            with open(default_player_file, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    discord_id = int(row['id'])
+                    if not await Player.exists(id=discord_id):
+                        users.append(Player(id=discord_id,
                                             steamid64=int(row['steamid64']),
                                             mode=Mode(row['mode'])))
-                await Player.bulk_create(users)
-                logger.info('Imported %d users from %s', len(users),
-                            initial_user_file)
+            await Player.bulk_create(users)
+            logger.info('Imported %d players from %s', len(users),
+                        default_player_file)
 
         try:
             new, updated = await refresh_db_maps()
