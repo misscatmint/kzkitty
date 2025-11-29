390da86f25c917b5dcaaf88679d82d053feac5ad
catmint <catmint@kittyware.cc>
2025-11-29 15:26:39 -0600

    Add missing refactor of SteamError handling

--- kzkitty/api/steam.py
+++ kzkitty/api/steam.py
@@ -1,10 +1,7 @@
-import logging
 from urllib.parse import urlparse
 from xml.etree import ElementTree
 
 from aiohttp import ClientError, ClientSession
-
-logger = logging.getLogger('kzkitty.api.steam')
 
 class SteamError(Exception):
     pass
@@ -12,30 +9,21 @@ class SteamError(Exception):
 class SteamValueError(SteamError):
     pass
 
-class SteamHTTPError(SteamError):
-    pass
-
-class SteamXMLError(SteamError):
-    pass
-
 async def _get_steam_profile(url: str) -> ElementTree.Element:
     try:
         async with ClientSession() as session:
             async with session.get(url) as r:
                 if r.status != 200 or r.content_type != 'text/xml':
-                    logger.error("Couldn't get Steam profile (HTTP %d)",
-                                 r.status)
-                    raise SteamHTTPError
+                    raise SteamError("Couldn't get Steam profile (HTTP %d)"
+                                     % r.status)
                 text = await r.text()
-    except ClientError:
-        logger.exception("Couldn't get Steam profile")
-        raise SteamHTTPError
+    except ClientError as e:
+        raise SteamError("Couldn't get Steam profile") from e
 
     try:
         return ElementTree.fromstring(text)
-    except ElementTree.ParseError:
-        logger.exception("Couldn't parse Steam profile XML")
-        raise SteamXMLError
+    except ElementTree.ParseError as e:
+        raise SteamError("Couldn't parse Steam profile XML") from e
 
 async def steamid64_for_profile(url: str) -> int:
     u = urlparse(url)
@@ -46,39 +34,33 @@ async def steamid64_for_profile(url: str) -> int:
     xml = await _get_steam_profile(url)
     steamid64 = xml.find('steamID64')
     if steamid64 is None or steamid64.text is None:
-        logger.error('Malformed Steam profile XML (no steamid64)')
-        raise SteamXMLError
+        raise SteamError('Malformed Steam profile XML (no steamid64)')
     try:
         return int(steamid64.text)
-    except ValueError:
-        logger.exception('Malformed Steam profile XML (bad steamid64)')
-        raise SteamXMLError
+    except ValueError as e:
+        raise SteamError('Malformed Steam profile XML (bad steamid64)') from e
 
 async def avatar_for_steamid64(steamid64: int) -> bytes:
     url = f'https://steamcommunity.com/profiles/{steamid64}?xml=1'
     xml = await _get_steam_profile(url)
     avatar = xml.find('avatarFull')
     if avatar is None or avatar.text is None:
-        logger.error('Malformed Steam profile XML (no avatar)')
-        raise SteamXMLError
+        raise SteamError('Malformed Steam profile XML (no avatar)')
 
     try:
         async with ClientSession() as session:
             async with session.get(avatar.text) as r:
                 if r.status != 200:
-                    logger.error("Couldn't get Steam profile (HTTP %d)",
-                                 r.status)
-                    raise SteamError
+                    raise SteamError("Couldn't get Steam avatar (HTTP %d)"
+                                     % r.status)
                 return await r.content.read()
-    except ClientError:
-        logger.exception("Couldn't get Steam profile")
-        raise SteamError
+    except ClientError as e:
+        raise SteamError("Couldn't get Steam avatar") from e
 
 async def name_for_steamid64(steamid64: int) -> str:
     url = f'https://steamcommunity.com/profiles/{steamid64}?xml=1'
     xml = await _get_steam_profile(url)
     steam_id = xml.find('steamID')
     if steam_id is None or steam_id.text is None:
-        logger.error('Malformed Steam profile XML (no steamID)')
-        raise SteamXMLError
+        raise SteamError('Malformed Steam profile XML (no steamID)')
     return steam_id.text
